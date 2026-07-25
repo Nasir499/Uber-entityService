278fffeba1db9378c6978d974cdb5218220f6c68
Nasir <nasirhossain1070@gmail.com>
2026-07-25 19:13:07 +0530

    Added basic models

---
+++ src/main/java/com/example/uberentityservice/UberEntityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.uberentityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberEntityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberEntityServiceApplication.class, args);
+    }
+
+}
